9d073f50cb8d88d5535c921aeca6b2dce472603d
Mitesh-Kumavat <miteshkumavat777@gmail.com>
2025-01-19 17:27:15 +0530

    api url change

--- scripts/util.js
+++ scripts/util.js
@@ -1,6 +1,6 @@
 
-// export const API_BASE_URL = "https://flask-movieverse.onrender.com";
-export const API_BASE_URL = "http://127.0.0.1:5000";
+export const API_BASE_URL = "https://flask-movieverse.onrender.com";
+// export const API_BASE_URL = "http://127.0.0.1:5000";
 
 export async function fetchMovieImage(imdbId) {
   const OMDB_API_KEY = '1e64c9b4';
