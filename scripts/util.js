dbafff9c8f0fca1a6b3c91913372a72f8cc9dca0
Mitesh-Kumavat <miteshkumavat777@gmail.com>
2025-01-23 10:46:56 +0530

    port change

--- scripts/util.js
+++ scripts/util.js
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = "https://flask-movieverse.onrender.com";
-export const API_BASE_URL = "http://127.0.0.1:5000";
+export const API_BASE_URL = "https://flask-movieverse.onrender.com";
+// export const API_BASE_URL = "http://127.0.0.1:5000";
 
 export const userId = localStorage.getItem("userId")
 
